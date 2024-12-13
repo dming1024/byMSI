7bee370dae68ecf042cf761f3fa311ca156f7e58
fan_qiangqiang <fan_qiangqiang@wuxiapptec.com>
2024-12-13 10:59:05 +0800

    byMSI

--- byMSI.R
+++ byMSI.R
@@ -1,20 +1,5 @@
 
-# byMSI 
-
-Bayesian Inference of Microsatellite Instability Status Based on Indel Variations in Tumor Genomes
-
-## optional: update parameters
-
-+ `lambda_MSI`
-+ `lambda_MSS`
-+ `p_msi`
-
-you can update these parameters to adapt your dataset or using them by default.
-
-
-## Usage
-
-```R
+#byMSI 
 
 p_msi=0.05
 posterior_probability<-function(len_indels,lambda_MSI,lambda_MSS,p_msi){
@@ -42,8 +27,3 @@ MSIp<-function(sequence,allele_sequence,prevalence=0.05){
 
 #input the length of indels in chr3:124951171
 p_ = MSIp(10,5)
-
-```
-
-+ The null hypothesis (`H0`) is rejected if the `p_` < 0.05, in which case the sample is considered to not be MSI-H
-+ Alternative hypothesis (`H1`) assumes the sample is not MSI-H
